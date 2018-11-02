01f9bc81765576f4c96e4d82aebc67b3ccd152ca
Garrett Tucker <garrettatucker@gmail.com>
2018-11-01 21:12:21 -0500

    final fixes

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def index():
 
 
 
+
 @app.route("/", methods=["POST"])
 def check_info():
     username = request.form["username"]
@@ -16,70 +17,33 @@ def check_info():
     email = request.form["email"]
 
     username_error = ""
-    first_password_error = ""
-    second_password_error = ""
+    password_error = ""
+    password_match_error = ""
     email_error = ""
 
-
-    def filled_out(input):
-        if input == "":
-            return False
-        else:   
-            return True
-
-    def no_spaces(input):
-        if " " in input:
-            return False
-        else:
-            return True
-
-    def email_verify(input):
-        if "@" and "." in input:
-            return True
-        else:
-            return False
-
-    def username_validation(name):
-        if filled_out(name) == False or no_spaces(name) == False:
-            username_error = "This field must be filled out"
-            username = "" 
-        elif len(name) < 3 or len(name) > 20:
-                username_error = "This field must be between 3 and 20 characters"
-                username = ""
-        else:
-            return first_password_validation(pass1)
-    def first_password_validation(password):
-        if filled_out(password) == False or no_spaces(password) == False:
-            first_password_error = "This field must be filled out with no spaces"
-            pass1 = ""
-        elif len(password) < 3 or len(password) > 20:
-                first_password_error = "This field must be between 3 and 20 characters"
-                pass1 = ""
-        else:
-            return second_password_validation(pass2)
-    def second_password_validation(verify):
-        if filled_out(verify) == False or no_spaces(verify) == False:
-            second_password_error = "This field must be filled out with no spaces"
-            pass1 = ""
-            pass2 = ""
-        elif len(verify) < 3 or len(verify) > 20:
-            second_password_error = "This field must be between 3 and 20 characters"
-            pass1 = ""
-            pass2 = ""
-        else:
-            return email_validation(email)
-    def email_validation(address):
-        if filled_out(address) == False or no_spaces(address) == False:
-            email_error = "This field must be filled out with no spaces"
-            email = ""
-        elif len(address) < 3 or len(address) > 20:
-                email_error = "This field must be between 3 and 20 characters"
-                email = ""
-        else:
-           return welcome(username)
-    def welcome(name):
-        return "<h1>Welcome " + name + "!</h1>"
-
-    return username_validation(username)
+    if len(username) < 3 or len(username) > 20:
+        username_error = "usernames cannot be blank and must be between 3 and 20 characters"
+        username = ""
+    elif " " in username:
+        username_error = "usernames cannot contain any spaces"
+        username = ""
+    if len(pass1) < 3 or len(pass1) > 20 or " " in pass1:
+        password_error = "passwords cannot be blank and must be between 3 and 20 characters"
+        pass1 = ""
+    elif pass2 != pass1:
+        password_error = "passwords must match"
+        pass2 = ""
+    if len(email) != 0 and len(email) < 3 or len(email) > 20:
+        email_error = "emails need to be between 3 and 20 characters in length"
+        email = ""
+    elif " " in email or "@" not in email and "." not in email:
+        email_error = "email is not formatted correctly"
+        email = ""
+    if not username_error and not password_error and not email_error:
+        return "<h1>Welcome " + username + "!</h1>"
+    else:
+        return render_template("form.html" , username_error = username_error , password_error = password_error, password_match_error = password_match_error, email_error = email_error, username = username, pass1 = pass1, pass2 = pass2, email = email)
+def welcome(name):
+    return "<h1>Welcome " + name + "!</h1>"
 
 app.run()
